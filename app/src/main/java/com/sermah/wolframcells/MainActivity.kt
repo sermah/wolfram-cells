4e8ab90452d2f1697f6fbd4060c858cda9d13e75
sermah <2004mah@gmail.com>
2022-08-22 11:03:32 +0300

    Added ability to change Wolfram Rules

--- app/src/main/java/com/sermah/wolframcells/MainActivity.kt
+++ app/src/main/java/com/sermah/wolframcells/MainActivity.kt
@@ -1,29 +1,24 @@
 package com.sermah.wolframcells
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.gestures.detectDragGestures
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.foundation.gestures.detectTransformGestures
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import com.sermah.wolframcells.components.CellGrid
 import com.sermah.wolframcells.data.OneDimCellData
 import com.sermah.wolframcells.data.WfRule
 import com.sermah.wolframcells.ui.theme.WolframCellsTheme
-import kotlinx.coroutines.launch
-import kotlin.math.ceil
-import kotlin.math.floor
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,35 +30,72 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    val cells by remember {
-                        mutableStateOf(
-                            OneDimCellData(
-                                width = 1000,
-                                height = 1000,
-                                statesCount = 2,
-                                evolveRule = WfRule(30)::execute
-                            )
+                    val makeCellsForRule = { rule: UByte ->
+                        OneDimCellData(
+                            width = 1000,
+                            height = 1000,
+                            statesCount = 2,
+                            evolveRule = WfRule(rule)::execute
                         ).also {
-                            it.value[it.value.width/2, 0] = 1
-                            it.value.completeSimulation()
+                            it[it.width/2, 0] = 1
+                            it.completeSimulation()
                         }
+                    }
+
+                    var currentRule by remember {
+                        mutableStateOf(30.toUByte())
+                    }
+
+                    var cells by remember {
+                        mutableStateOf(
+                            makeCellsForRule.invoke(currentRule)
+                        )
                     }
 
                     var totalOffset by remember {
                         mutableStateOf(Offset(0f, 0f))
                     }
-                    val cellSize by remember {
+                    var cellSize by remember {
                         mutableStateOf(4f)
                     }
 
-                    CellGrid(
-                        data = cells,
-                        totalOffset = totalOffset,
-                        cellSize = cellSize.dp,
-                        modifier = Modifier.fillMaxSize().pointerInput(Unit) {
-                            detectDragGestures { _, dragAmount -> totalOffset -= dragAmount }
+                    var fieldValue by remember {
+                        mutableStateOf("30")
+                    }
+
+                    Column {
+                        Row (
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.SpaceAround,
+                            modifier = Modifier.padding(vertical = 8.dp, horizontal = 16.dp)
+                        ){
+                            TextField(value = fieldValue, modifier = Modifier.weight(1f), onValueChange = { str -> fieldValue = str })
+                            Button(modifier = Modifier.padding(start = 16.dp), onClick = {
+                                val newRule = fieldValue.toUIntOrNull()
+                                if (newRule != null && newRule < 256U) {
+                                    currentRule = newRule.toUByte()
+                                    cells = makeCellsForRule.invoke(currentRule)
+                                }
+                            }) {
+                                Text("Generate")
+                            }
                         }
-                    )
+                        CellGrid(
+                            data = cells,
+                            totalOffset = totalOffset,
+                            cellSize = cellSize.dp,
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .pointerInput(Unit) {
+                                    detectTransformGestures { centroid, pan, zoom, _ ->
+                                        totalOffset -= pan
+                                        totalOffset = (totalOffset + centroid) / cellSize
+                                        cellSize *= zoom
+                                        totalOffset = totalOffset * cellSize - centroid
+                                    }
+                                }
+                        )
+                    }
                 }
             }
         }
--- app/src/main/java/com/sermah/wolframcells/data/WfRule.kt
+++ app/src/main/java/com/sermah/wolframcells/data/WfRule.kt
@@ -1,7 +1,7 @@
 package com.sermah.wolframcells.data
 
 data class WfRule(
-    val rule: Byte
+    val rule: UByte
 ) {
     fun execute(l: Byte, c: Byte, r: Byte) = (rule.toInt() shr (
             (l.toInt() shl 2) or (c.toInt() shl 1) or (r.toInt())
